486b20234cedb47c7699e71209657c2a03566433
liangweihao <514990114@qq.com>
2017-10-21 02:03:26 -0500

    Update RadarView.java

--- RadarView.java
+++ RadarView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.RectF;
@@ -36,13 +37,13 @@ public class RadarView extends View {
     // 外部点的数量
     int outPointNum = 4;
     //    外部点的半径
-    int outPointRadius = 20;
+    int outPointRadius = 10;
     // 设置半径上的分割数量  默认为 1
-    int centerToPointStep = 3;
+    int centerToPointStep = 5;
     //     设置初始点的角度位置
-    int startFromAngle = 270;
+    int startFromAngle = 0;
     //    半径长度
-    int mRadius = 300;
+    float mRadius = 300;
 
     Paint pointPaint;
 
@@ -51,7 +52,7 @@ public class RadarView extends View {
         setWillNotDraw(false);
         pointPaint = new Paint();
         pointPaint.setAntiAlias(true);
-        pointPaint.setTextSize(50);
+        pointPaint.setTextSize(30);
         pointPaint.setStrokeWidth(2);
         pointPaint.setColor(Color.parseColor("#42A5F5"));
         pointPaint.setStyle(Paint.Style.FILL);
@@ -72,31 +73,201 @@ public class RadarView extends View {
         lineOutPoints(canvas);
         circleOutPoints(canvas);
         drawLastOutPointText(canvas);
+        calculateInnerPoints();
+        lineInnerPoints(canvas);
+        drawInnerPoints(canvas);
         canvas.restore();
     }
 
-    String[] outTexts = {
-            "Android", "PS", "Object-C", "PHP", "CSS3", "HTML", "JAVA","JavaScript","GO","Nodejs","MySql",
-            "NoSql", "Sqite3", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql",
-            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA","Android","GO","Nodejs","MySql"
+    /*
+    * 连接内部点
+    * */
+    private void lineInnerPoints(Canvas canvas) {
+        int size = mInnerPoints.size();
+//      有几条线就花几个点
+        Path path = new Path();
+        for (int i = 0; i < size; i++) {
+            PointF pointF = mInnerPoints.get(i);
+            float x = pointF.x;
+            float y = pointF.y;
+////           移动到某个点
+            if (i == 0) {
+                path.moveTo(x, y);
+            }
+            path.lineTo(x, y);
 
+        }
+
+        Paint pt = new Paint(pointPaint);
+        pt.setStrokeWidth(3);
+        pt.setStyle(Paint.Style.FILL_AND_STROKE);
+        pt.setColor(Color.parseColor("#6642A5F5"));
+        canvas.drawPath(path, pt);
+    }
+
+    /*
+    * 绘制内部点的形状
+    * */
+    private void drawInnerPoints(Canvas canvas) {
+
+        int size = mInnerPoints.size();
+        for (int i = 0; i < size; i++) {
+            PointF pointF = mInnerPoints.get(i);
+            Paint pt = new Paint(pointPaint);
+
+            pt.setColor(Color.parseColor("#E91E63"));
+//           这里控制内部点的半径
+            canvas.drawCircle(pointF.x, pointF.y, 5, pt);
+
+        }
+    }
+
+    SparseArray<PointF> mInnerPoints = new SparseArray<>();
+
+    /*
+    * 计算出所有的内部点
+    * */
+    private void calculateInnerPoints() {
+        mInnerPoints.clear();
+//  需要计算顶点的个数
+        for (int i = 0; i < outPointNum; i++) {
+//            获取到内部点的距离
+            float distance = 0;
+            distance = innerPointDistance[i];
+
+//           测试数据    随机点
+//            distance =(float) Math.random();
+//            distance = (float) (distance < 0.2 ? 0.2 : distance) * mRadius;
+
+
+//            求K 也就是斜率
+            PointF poutf = mLastOutPointArray.get(i);
+            float k = getGradient(poutf);
+            double x = 0, y = 0;
+            if (k != 0) {
+                x = distance / Math.sqrt(Math.pow(k, 2) + 1);
+//                确定斜率的正负关系
+                if (poutf.x < getCenterX()) {
+                    x = -x + getCenterX();
+                } else {
+                    x = x + getCenterX();
+                }
+                y = k * (x-getCenterX()) + getCenterY();
+            } else {
+//                斜率为0 点那个x为重点的时候 求 y  当y为重点的时候求 x
+//                 确定x值
+                if (poutf.x == getCenterX()){
+                    x = getCenterX();
+
+
+//                    这里需要注意  因为如果以中心为坐标点的话 那么 其坐标的正负和真实的是相反的因此需要进行正负计算
+                    if (poutf.y > getCenterY()){
+                        y = getCenterY() - distance;
+                    }
+                    if (poutf.y < getCenterY()){
+                        y = getCenterY() + distance;
+                    }
+
+
+                }
+
+//                确定Y值
+                if (poutf.y == getCenterY()){
+                    y = getCenterY();
+                    //                正方向
+                    if (poutf.x > getCenterX()){
+//                    正方向的点为+距离
+                        x = getCenterX() + distance;
+
+                    }
+                    if (poutf.x < getCenterX()){
+                        x = getCenterX() - distance;
+
+                    }
+                }
+            }
+////           相对于原点的 x
+//            判断X的正负
+
+////           相对于原点的 Y
+//            double y = k != 0 ? k * x : (poutf.y < getCenterY() ? getCenterY() - poutf.y : poutf.y - getCenterY());
+//            存放绝对坐标值
+            mInnerPoints.put(i, new PointF((float) x, (float) y ));
+
+        }
+    }
+
+    /*
+    * 求斜率 其实都是一样的 因为后来对其平方以后还是正 但这么做了一个简单的斜率封装
+    * */
+    private float getGradient(PointF pointF) {
+        float rx = pointF.x;
+        float ry = pointF.y;
+
+        float k = 0;
+//        第一 三象限
+        if (rx > getCenterX() && ry < getCenterY() || rx < getCenterX() && ry > getCenterY()) {
+
+            float dx = rx - getCenterX();
+            float dy = ry - getCenterY();
+            return -dy / dx;
+
+        }
+        //        第2⃣️ 四象限
+        if (rx < getCenterX() && ry < getCenterY() || rx > getCenterX() && ry > getCenterY()) {
+            float dx = rx - getCenterX();
+            float dy = ry - getCenterY();
+            return -dy / dx;
+
+        }
+//        其余的都是在主轴
+        return k;
+    }
+
+
+    String[] outTexts = {
+            "Android", "PS", "Object-C", "PHP", "CSS3", "HTML", "JAVA", "JavaScript", "GO", "Nodejs", "MySql",
+            "NoSql", "Sqite3", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql",
+            "JavaScript", "PHP", "Object-C", "Swift", "CSS3", "HTML", "JAVA", "Android", "GO", "Nodejs", "MySql"
+
+    };
+    //    内部点的坐标位置
+    /*
+     确定距离原点的长度
+     计算出响应的xy
+     计算出准确的坐标位置
+     绘制坐标点
+     连接坐标点
+     填充颜色
+    *
+    * */
+    float[] innerPointDistance = {
+            255, 190, 10, 299, 50, 200, 110, 90, 25, 190, 231,
+            255, 190, 10, 299, 50, 200, 110, 90, 25, 190, 231,
+            255, 190, 10, 299, 50, 200, 110, 90, 25, 190, 231,
+            255, 190, 10, 299, 50, 200, 110, 90, 25, 190, 231,
+            255, 190, 10, 299, 50, 200, 110, 90, 25, 190, 231,
+            255, 190, 10, 299, 50, 200, 110, 90, 25, 190, 231,
+            255, 190, 10, 299, 50, 200, 110, 90, 25, 190, 231,
+            255, 190, 10, 299, 50, 200, 110, 90, 25, 190, 231,
+            255, 190, 10, 299, 50, 200, 110, 90, 25, 190, 231
     };
     float mOutPointToTextDistance = 20;
 
@@ -110,13 +281,13 @@ public class RadarView extends View {
             if (i < outTexts.length) {
                 PointF rp = getRelativePositionByText(outTexts[i], pointF);
 //                绘制选中的字体颜色
-                if (mCurrentIndex!=-1&&(mCurrentIndex + 1)%(outPointNum )== i) {
+                if (mCurrentIndex != -1 && (mCurrentIndex) % (outPointNum) == i) {
                     Paint pt = new Paint(pointPaint);
                     pt.setColor(Color.RED);
-                    canvas.drawText(outTexts[i], rp.x, rp.y, pt);
+                    canvas.drawText(outTexts[i] + "-" + i, rp.x, rp.y, pt);
 
                 } else {
-                    canvas.drawText(outTexts[i], rp.x, rp.y, pointPaint);
+                    canvas.drawText(outTexts[i] + "-" + i, rp.x, rp.y, pointPaint);
                 }
             }
         }
@@ -128,6 +299,7 @@ public class RadarView extends View {
     *
     * */
     private PointF getRelativePositionByText(String text, PointF pointF) {
+//        原来实际的坐标点
         float rx = pointF.x;
         float ry = pointF.y;
 //
@@ -239,7 +411,7 @@ public class RadarView extends View {
         float pnum = 360f / outPointNum;
         int mCount = 0;
 //每段的长度
-        int istep = mRadius / centerToPointStep;
+        float istep = mRadius / centerToPointStep;
         for (int j = 0; j < centerToPointStep; j++) {
             for (int i = 0; i < outPointNum; i++) {
                 float mp = pnum * i;
@@ -258,7 +430,8 @@ public class RadarView extends View {
                 * 调整文字的位置
                 * */
                 if (j == centerToPointStep - 1) {
-                    calculateOutPointRefer((i + 1) % outPointNum,
+//                    这里修复了一个问题 源于 文字和应该持有的索引位置不正确
+                    calculateOutPointRefer((i) % outPointNum,
                             rx,
                             ry
                     );
@@ -311,7 +484,7 @@ public class RadarView extends View {
                         long la = System.currentTimeMillis() - l;
                         mCurrentIndex = i;
                         invalidate();
-                        Toast.makeText(getContext(),outTexts[(mCurrentIndex+1)%outPointNum] , Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), outTexts[(mCurrentIndex) % outPointNum] + " index=" + i, Toast.LENGTH_SHORT).show();
                         break;
                     }
                 }
@@ -379,6 +552,10 @@ public class RadarView extends View {
 
     public void setOutPointRefer(int size) {
         this.pointPaint.setTextSize(size);
+        invalidate();
+    }
+    public void setInnerPointDistance(float... inces){
+        this.innerPointDistance = inces;
         invalidate();
     }
 
